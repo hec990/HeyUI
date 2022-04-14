4b5dc388725f812f9a2e41d387f911ea7130f0c9
xiaohehe <1296522554@qq.com>
2022-04-15 02:59:49 +0800

    基本完成Switch组件

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,13 +1,52 @@
 <template>
-  Switch
+  <button class="hey-switch" :class="{'hey-checked':checked}" @click="toggle">
+    <span></span>
+  </button>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
-  name: "hey-switch"
+  name: "hey-switch",
+  setup(){
+    const checked = ref(false)
+    const toggle = () =>{
+      checked.value = !checked.value
+    }
+    return {
+      checked,
+      toggle
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.hey-switch{
+  height: 22px;
+  width: 22px*2;
+  border: none;
+  background-color: #E3E3E3;
+  border-radius: 22px/2;
+  position: relative;
+  border-radius: 20px;
+  cursor: pointer;
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: 18px;
+    width: 18px;
+    background-color: white;
+    border-radius: 18px/2;
+    transition: left 250ms;
+    border-radius: 20px;
+  }
+  &.hey-checked {
+    background-color: #8333a8;
+  }
+  &.hey-checked > span{
+    left: calc(100% - #{18px} - 2px);
+  }
+}
 </style>
