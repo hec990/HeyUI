8d48f2c6e371c45674dcdde67109c7b9cde6d25e
xiaohehe <1296522554@qq.com>
2022-04-24 14:52:40 +0800

    Input：完成默认输入框

--- src/lib/hey-input.vue
+++ src/lib/hey-input.vue
@@ -1,5 +1,7 @@
 <template>
-  <input type="text">
+  <div class="hey-input-container">
+    <input type="text" class="hey-input">
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.hey-input-container {
+  font-size: 12px;
+  display: inline-block;
+  > .hey-input {
+    height: 32px;
+    border: 1px solid #999;
+    border-radius: 4px;
+    padding: 0 8px;
+    font-size: inherit;
+    outline:none;
+    &:hover,&:focus { border-color: #8333a8; }
+  }
+}
 </style>
